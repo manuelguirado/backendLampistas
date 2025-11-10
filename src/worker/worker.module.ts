634e2e5cd0dd7aceca4950ea38b6ca3b3c1a4386
manuelguirado <manu22.mgb@gmail.com>
2025-11-10 18:04:40 +0100

    feat: add AuthModule import to WorkerModule for authentication integration

--- src/worker/worker.module.ts
+++ src/worker/worker.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WorkerController } from './worker.controller';
 import { WorkerService } from './worker.services';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [WorkerController],
   providers: [WorkerService],
   exports: [WorkerService],
